ad6ce800a7ab89eedb3aed71fc5bd1a1f52f146b
NchegeMoringa <narikso.chege@student.moringaschool.com>
2021-05-02 22:32:23 +0300

    game js using query selector
    game js using query selector

---
+++ script/game.js
@@ -0,0 +1,6 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progresstext');
+const scoreText = document.querySelector('#score');
+const question = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
