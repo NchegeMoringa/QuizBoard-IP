246072df10596b979dcd0445077453ac2fed5e55
NchegeMoringa <narikso.chege@student.moringaschool.com>
2021-05-02 23:48:59 +0300

    bug fix on JS CSS
    bug fix on JS CSS

--- script/game.js
+++ script/game.js
@@ -77,31 +77,31 @@ getNewQuestions = () =>{
         return window.location.assign('/end.html') // the close html page.
     }
     questionCounter++
-    progressText.innerText = `Question ${questionCounter}' of ${MAX_QUESTIONS}`
-    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%` //calculate the question user is on
     
     const questionsIndex = Math.floor(Math.random() *availableQuestions.length)
-    currentQuestion = availableQuestions[questionsIndex]
+    currentQuestion = availableQuestions[questionsIndex] // keep track of queiz i'm on
     question.innerHTML = currentQuestion.question
 
     choices.forEach(choice=>{
         const number = choice.dataset['number']
-        choice.innerText = currentQuestion['choice' +number]
+        choice.innerText = currentQuestion['choice' + number]
     })
 
-    availableQuestions.splice(questionsIndex,1)
+    availableQuestions.splice(questionsIndex, 1)
 
     acceptingAnswers = true
 
 }
 
-choice.forEach(choice =>{
-    choice.addEventListener('click', e=>{
+choices.forEach(choice =>{
+    choice.addEventListener('click', e =>{
         if(!acceptingAnswers) return
         
         acceptingAnswers = false
         const selectedChoice = e.target
-        const selectedAnswer = selectedChoice.dataset('number')
+        const selectedAnswer = selectedChoice.dataset['number']
 
         let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' :
         'incorrect'
